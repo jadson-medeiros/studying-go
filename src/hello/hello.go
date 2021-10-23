9c8e07f2cebad7a1d1199b911ad788e4921818ca
jadsonmedeirosjunior <developer.medeiros@hotmail.com>
2021-10-23 18:08:48 -0300

    working with variables

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world with Go")
+
+	name := "GitHub"
+	version := 1.0
+
+	fmt.Println("Hello, sr.", name)
+	fmt.Println("This program is available to version", version)
+
+	fmt.Println("1- Start Monitoring")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
+
+	var command int
+	fmt.Scan(&command)
+	fmt.Println("The chosen command was", command)
 }
