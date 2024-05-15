544d7c4b8488b3ea149fb6174882b4cfa3117150
Jadson Medeiros <developer.medeiros@gmail.com>
2024-05-15 18:39:13 -0300

    selects with gorm

--- src/database/gorm/main.go
+++ src/database/gorm/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -23,17 +24,32 @@ func main() {
 	db.AutoMigrate(&Product{})
 
 	// create
-	db.Create(&Product{
-		Name: "Notebook",
-		Price: 1000.00,
-	})
+	// db.Create(&Product{
+	// 	Name: "Notebook",
+	// 	Price: 1000.00,
+	// })
 
-	//create batch
-	products := []Product{
-		{Name: "Laptop", Price: 1000.00},
-		{Name: "Mouset", Price: 50.00},
-		{Name: "Book", Price: 100.00},
+	// //create batch
+	// products := []Product{
+	// 	{Name: "Laptop", Price: 1000.00},
+	// 	{Name: "Mouse", Price: 50.00},
+	// 	{Name: "Book", Price: 100.00},
+	// }
+
+	// db.Create(&products)
+
+	// select one
+	// var product Product
+	// //db.First(&product, 1)
+	// //fmt.Println(product.Name)
+	// db.First(&product, "name = ?", "Mouse")
+	// fmt.Println(product)
+
+	// select all
+	var products []Product
+
+	db.Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
 	}
-
-	db.Create(&products)
 }
