c52d41fb1bd130e4b5927c9ec2aeaee1cce57937
Jadson Medeiros <developer.medeiros@gmail.com>
2024-05-14 23:35:34 -0300

    testing mocks

--- src/testing/2/tax_test.go
+++ src/testing/2/tax_test.go
@@ -1,6 +1,7 @@
 package tax
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,19 @@ func TestCalculateTax(t *testing.T) {
 	tax, err = CalculateTax(0)
 	assert.Error(t, err, "amount must be greater than zero.")
 	assert.Equal(t, 0.0, tax)
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil).Twice()
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+	err = CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error calculating tax")
+
+	repository.AssertExpectations(t)
 }
